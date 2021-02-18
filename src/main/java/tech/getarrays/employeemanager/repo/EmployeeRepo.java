e0d0821bc9e4c17328f60ef41dc0945e4dd2f787
khairil <khairsfwrdev@gmail.com>
2021-02-18 15:40:25 +0800

    Exception Handling

--- src/main/java/tech/getarrays/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/tech/getarrays/employeemanager/repo/EmployeeRepo.java
@@ -9,5 +9,5 @@ public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
     void deleteEmployeeById(long id);
 
-    Optional findEmployeeById(Long id);
+    Optional<Employee> findEmployeeById(Long id);
 }
